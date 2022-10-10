2f29d4491a3d64bca7a84d3b3c37fdaaf6fb9e5e
Kevin Quinn <kpquinn2@wisc.edu>
2022-10-10 02:46:58 -0500

    Switched to the sagi Slack api client; minimal example functional

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,49 @@
  */
 // src/index.ts
 import { Hono } from 'hono'
-const app = new Hono()
+const SlackREST = require('@sagi.io/workers-slack')
 
-app.get('/', (c) => c.text('Hello! Hono! Tono!'))
+const channelId = "CP1S57DAB"  // conference-2019
 
-export default app
+const honoApp = new Hono()
+
+honoApp.get('/', (c) => c.text('Hello! Hono! Tono!'))
+
+honoApp.post('/slack/events', async (c) => {
+	const body = await c.req.json()
+	console.log(`BODY: ${JSON.stringify(body)}`)
+
+	if ("challenge" in body) {
+		// https://api.slack.com/apis/connections/events-api#the-events-api__subscribing-to-event-types__events-api-request-urls
+		return c.json({
+			challenge: body.challenge
+		})
+	} 
+
+	const botAccessToken  = c.env.SLACK_BOT_TOKEN;
+	const slackClient = new SlackREST({ botAccessToken })
+	// TODO (optional): validate Slack request is from Slack with signing secret
+
+	// TODO: parse out request events into the different subtypes
+
+	const slackEvent = body.event;
+
+	try {
+		// Call chat.postMessage with the built-in client
+		const result = await slackClient.chat.postMessage({
+		  channel: channelId,
+		  text: `Oh boy an event! ${JSON.stringify(slackEvent)}.`
+		});
+		console.log(result);
+	  }	
+	  catch (error) {
+		console.log(error);
+		return c.json({ ok: false, error: error.message})
+	  }
+
+	  return c.json({ ok: true })
+})
+
+export default honoApp
+
 
